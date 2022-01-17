699864297a362401ef367812e691c4107addb282
Welly S. L <welly.liem@ardomusnet.com>
2022-01-17 17:52:23 +0800

    S24-V376: Adding Auto-Logout
    auth.service.ts:
    - remove unnecessary services
    - replace autoLogout() with setLogoutTimer()
    - add clearLogoutTimer()
    
    auth.effeccts.ts
    - inject AuthService
    - implement setLogoutTimer() & clearLogoutTimer() on several effects
    - add navigate to auth page on logout action

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -8,6 +8,7 @@ import { environment } from "src/environments/environment";
 
 import * as AuthActions from './auth.actions';
 import { User } from '../user.model';
+import { AuthService } from "../auth.service";
 
 export interface AuthResponseData {
   kind: string;
@@ -72,6 +73,9 @@ export class AuthEffects {
           returnSecureToken: true
         }
       ).pipe(
+        tap(resData => {
+          this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+        }),
         map(resData => {
           return handleAuthentication(
             +resData.expiresIn, 
@@ -101,6 +105,9 @@ export class AuthEffects {
         }
       )
       .pipe(
+        tap(resData => {
+          this.authService.setLogoutTimer(+resData.expiresIn *1000);
+        }),
         map(resData => {
           return handleAuthentication(
             +resData.expiresIn, 
@@ -123,7 +130,7 @@ export class AuthEffects {
    */
   @Effect({dispatch: false})
   authRedirect = this.actions$.pipe(
-    ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT),
+    ofType(AuthActions.AUTHENTICATE_SUCCESS),
     tap(() => {
       this.router.navigate(['/']);
     })
@@ -152,6 +159,10 @@ export class AuthEffects {
   
       if (loadedUser.token) {
         // this.user.next(loadedUser);
+        const expirationDuration =
+          new Date(userData._tokenExpirationDate).getTime() -
+          new Date().getTime();
+        this.authService.setLogoutTimer(expirationDuration);
         return new AuthActions.AuthenticateSuccess({
           email: loadedUser.email,
           userId: loadedUser.id,
@@ -172,7 +183,9 @@ export class AuthEffects {
   authLogout = this.actions$.pipe(
     ofType(AuthActions.LOGOUT),
     tap(() => {
-      localStorage. removeItem('userData');
+      this.authService.clearLogoutTimer();
+      localStorage.removeItem('userData');
+      this.router.navigate(['/auth']);
     })
   );
 
@@ -184,5 +197,6 @@ export class AuthEffects {
     private actions$: Actions,
     private http: HttpClient,
     private router: Router,
+    private authService: AuthService
   ) {}
 }
