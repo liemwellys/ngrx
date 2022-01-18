e4814ddac9fd0cb3e82b0718a4b45489a9e75c1b
Welly S. L <welly.liem@ardomusnet.com>
2022-01-18 17:24:58 +0800

    S24-V382: Fetching Recipes & Using the Resolver - added fetchRecipes effect to recipe.effects.ts - import RecipeEffects class to app.module.ts - added FetchRecipes() action to recipe.action.ts - repalace service usage with dispatch store on recipe-resolver.service.ts

---
+++ src/app/recipes/store/recipe.effects.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { switchMap, map } from "rxjs/operators";
+
+import * as RecipesActions from './recipe.actions';
+import { Recipe } from "../recipe.model";
+
+@Injectable()
+export class RecipeEffects {
+  @Effect()
+  fetchRecipes = this.actions$.pipe(
+    ofType(RecipesActions.FETCH_RECIPES),
+    switchMap(() => {
+      return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-9d503-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json'
+      )
+    }),
+    map(recipes => {
+      return recipes.map(recipe => {
+        return {
+          ...recipe,
+          ingredients: recipe.ingredients ? recipe.ingredients : []
+        };
+      });
+    }),
+    map(recipes => {
+      return new RecipesActions.SetRecipes(recipes);
+    })
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient
+  ) {}
+}
